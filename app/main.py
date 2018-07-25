ae71365d7159fddcc13a9b424cf54ef71261e07c
igorxxl <vip.turcevich3@gmail.com>
2018-07-25 20:56:44 +0300

    modified console parser

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
-from parser import Parser
+from parser import Parser, ParserConstants
 import sys
 
 
 def main():
     parser_ = Parser(sys.argv)
-    print(parser_.parse())
+    args_dict = parser_.get_args_dict()
+
+    # get target for processing
+    target = args_dict.pop(ParserConstants.TARGET)
+    if not target:
+        parser_.show_usage(dest=sys.stderr)
+        exit(1)
+
+    # TODO: process target
+
+    # get action for perform target operation
+    action = args_dict.pop(ParserConstants.ACTION)
+    if not action:
+        parser_.show_usage(dest=sys.stderr, level=2)
+        exit(1)
+
+    # TODO: process action
 
 
 if __name__ == '__main__':
--- app/parser.py
+++ app/parser.py
@@ -1,110 +1,149 @@
-import argparse as ap
+"""This module have a Parser class for processing users's console input"""
+from argparse import ArgumentParser
 
 
-class Parser:
-    """Класс для обработки консольного ввода от пользователя."""
-    # константы, использующиеся парсером
+class ParserConstants:
+    """Constants, which represent main parser commands and settings"""
     DESCRIPTION = 'Calistra - task tracker'
+    TARGET = 'target'
+    TARGET_HELP = 'target for working'
+    ACTIVE_TARGET = None
+    ACTION = 'action'
+    ADD = 'add'
+    DELETE = 'del'
+    EDIT = 'edit'
+
+
+class UserParserConstants:
+    """Constants, which represent user parser commands and settings"""
+    USER = 'user'
+    USER_HELP = 'working with user'
+    USER_SUBPARSER_HELP = 'action with user'
     NICKNAME = 'nick'
     NICKNAME_HELP = "user's nickname"
     PASSWORD = 'pasw'
     PASSWORD_HELP = "user's password"
-    SUBPARSERS_HELP = 'target'
-    USER = 'user'
-    USER_HELP = 'working with user'
-    USER_SUBPARSER_HELP = 'operation with user'
-    ADD = 'add'
     ADD_USER_HELP = 'add new user'
-    ADD_TASK_HELP = 'add new task'
+    DELETE_USER_HELP = 'delete existing user'
     LOGIN = 'login'
     LOGIN_HELP = "login with nickname and password"
-    DELETE = 'del'
-    DELETE_USER_HELP = 'delete existing user'
-    DELETE_TASK_HELP = 'delete existing task'
+
+
+class TaskParserConstants:
+    """Constants, which represent task parser commands and settings"""
     TASK = 'task'
     TASK_HELP = 'working with tasks'
-    EDIT = 'edit'
+    ADD_TASK_HELP = 'add new task'
+    DELETE_TASK_HELP = 'delete existing task'
     EDIT_TASK_HELP = 'edit task'
     TASK_SUBPARSER_HELP = 'work with task'
 
+
+class Parser:
+    """Class for processing console input"""
+
     def __init__(self, console_args):
+        """Init parser's attributes and create parsers"""
         self.args = console_args
-        self.parser = ap.ArgumentParser(
-            argument_default=ap.SUPPRESS,
-            description=Parser.DESCRIPTION
-        )
-        self.subparsers = self.parser.add_subparsers(
-            help=Parser.SUBPARSERS_HELP
-        )
+        self.parser = ArgumentParser(
+            description=ParserConstants.DESCRIPTION)
+        subparsers = self.parser.add_subparsers(
+            dest=ParserConstants.TARGET,
+            help=ParserConstants.TARGET_HELP)
 
-        if len(self.args) > 1:
-            # субпарсеры обработки данных о пользователе
-            user_parser = self.subparsers.add_parser(
-                name=Parser.USER,
-                help=Parser.USER_HELP
-            )
-            task_parser = self.subparsers.add_parser(
-                name=Parser.TASK,
-                help=Parser.TASK_HELP
-            )
-            if self.args[1] == Parser.USER:
-                user_subparsers = user_parser.add_subparsers(
-                    help=Parser.USER_SUBPARSER_HELP
-                )
-                register_subparsers = user_subparsers.add_parser(
-                    name=Parser.ADD,
-                    help=Parser.ADD_USER_HELP
-                )
-                register_subparsers.add_argument(
-                    dest=Parser.NICKNAME,
-                    help=Parser.NICKNAME_HELP
-                )
-                register_subparsers.add_argument(
-                    dest=Parser.PASSWORD,
-                    help=Parser.PASSWORD_HELP
-                )
-                login_subparsers = user_subparsers.add_parser(
-                    name=Parser.LOGIN,
-                    help=Parser.LOGIN_HELP
-                )
-                login_subparsers.add_argument(
-                    dest=Parser.NICKNAME,
-                    help=Parser.NICKNAME_HELP
-                )
-                login_subparsers.add_argument(
-                    dest=Parser.PASSWORD,
-                    help=Parser.PASSWORD_HELP
-                )
-                delete_subparsers = user_subparsers.add_parser(
-                    name=Parser.DELETE,
-                    help=Parser.DELETE_USER_HELP
-                )
-                delete_subparsers.add_argument(
-                    dest=Parser.NICKNAME,
-                    help=Parser.NICKNAME_HELP
-                )
-                delete_subparsers.add_argument(
-                    dest=Parser.PASSWORD,
-                    help=Parser.PASSWORD_HELP
-                )
+        # create next level parsers for different targets
+        user_parser = subparsers.add_parser(
+            name=UserParserConstants.USER,
+            help=UserParserConstants.USER_HELP)
+        task_parser = subparsers.add_parser(
+            name=TaskParserConstants.TASK,
+            help=TaskParserConstants.TASK_HELP)
 
-            elif self.args[1] == Parser.TASK:
-                # субпарсеры для обработки данных о задачах
-                task_subparsers = task_parser.add_subparsers(
-                    help=Parser.TASK_SUBPARSER_HELP
-                )
-                crt_subparsers = task_subparsers.add_parser(
-                    name=Parser.ADD,
-                    help=Parser.ADD_TASK_HELP
-                )
-                edit_subparsers = task_subparsers.add_parser(
-                    name=Parser.EDIT,
-                    help=Parser.EDIT_TASK_HELP
-                )
-                del_subparsers = task_subparsers.add_parser(
-                    name=Parser.DELETE,
-                    help=Parser.DELETE_TASK_HELP
-                )
+        # check console args and create subparsers for necessary args
+        if UserParserConstants.USER in self.args:
+            self._create_user_subparsers(user_parser)
+            # save user parser as active target for show right error message
+            self.active_target = user_parser
+        elif TaskParserConstants.TASK in self.args:
+            self._create_task_subparsers(task_parser)
+            # save target parser as active target for show right error message
+            self.active_target = task_parser
 
-    def parse(self):
-        return self.parser.parse_args()
+    @staticmethod
+    def _create_user_subparsers(user_parser):
+        """Create subparsers for processing user's data"""
+        user_subparsers = user_parser.add_subparsers(
+            dest=ParserConstants.ACTION,
+            help=UserParserConstants.USER_SUBPARSER_HELP)
+
+        # calistra user add <nickname> <password>
+        add_subparsers = user_subparsers.add_parser(
+            name=ParserConstants.ADD,
+            help=UserParserConstants.ADD_USER_HELP)
+        add_subparsers.add_argument(
+            dest=UserParserConstants.NICKNAME,
+            help=UserParserConstants.NICKNAME_HELP)
+        add_subparsers.add_argument(
+            dest=UserParserConstants.PASSWORD,
+            help=UserParserConstants.PASSWORD_HELP)
+
+        # calistra user login <nickname> <password>
+        login_subparsers = user_subparsers.add_parser(
+            name=UserParserConstants.LOGIN,
+            help=UserParserConstants.LOGIN_HELP)
+        login_subparsers.add_argument(
+            dest=UserParserConstants.NICKNAME,
+            help=UserParserConstants.NICKNAME_HELP)
+        login_subparsers.add_argument(
+            dest=UserParserConstants.PASSWORD,
+            help=UserParserConstants.PASSWORD_HELP)
+
+        # calistra user delete <nickname> <password>
+        delete_subparsers = user_subparsers.add_parser(
+            name=ParserConstants.DELETE,
+            help=UserParserConstants.DELETE_USER_HELP)
+        delete_subparsers.add_argument(
+            dest=UserParserConstants.NICKNAME,
+            help=UserParserConstants.NICKNAME_HELP)
+        delete_subparsers.add_argument(
+            dest=UserParserConstants.PASSWORD,
+            help=UserParserConstants.PASSWORD_HELP)
+
+    @staticmethod
+    def _create_task_subparsers(task_parser):
+        """Create subparsers for processing task's data"""
+        task_subparsers = task_parser.add_subparsers(
+            dest=ParserConstants.ACTION,
+            help=TaskParserConstants.TASK_SUBPARSER_HELP)
+
+        # TODO: подумать над атрибутами task
+
+        # calistra task add
+        add_subparsers = task_subparsers.add_parser(
+            name=ParserConstants.ADD,
+            help=TaskParserConstants.ADD_TASK_HELP)
+
+        # calistra task edit
+        edit_subparsers = task_subparsers.add_parser(
+            name=ParserConstants.EDIT,
+            help=TaskParserConstants.EDIT_TASK_HELP)
+
+        # calistra task delete
+        delete_subparsers = task_subparsers.add_parser(
+            name=ParserConstants.DELETE,
+            help=TaskParserConstants.DELETE_TASK_HELP)
+
+    def get_args_dict(self):
+        """Create a dict of args and values"""
+        return vars(self.parser.parse_args())
+
+    def show_usage(self, dest, level=1):
+        """
+        Show help message in case incorrect user's input
+        dest: output stream
+        level: subparsers level
+        """
+        if level == 1:
+            self.parser.print_help(dest)
+        elif level == 2:
+            self.active_target.print_help(dest)

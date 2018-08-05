b99a9bd0d4c19b9eab2005c815c1e7fe5cfa7d00
igorxxl <vip.turcevich3@gmail.com>
2018-08-05 17:06:00 +0300

    added setup.py for library

---
+++ lib/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import calistra_lib
+
+setup(
+    name='calistra_lib',
+    version=calistra_lib.__version__,
+    packages=find_packages(),
+    long_description="lib",
+    include_package_data=True,
+)

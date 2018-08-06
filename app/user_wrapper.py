9b66780f4ae5c2c56eef664c3fefa57248490f35
igorxxl <vip.turcevich3@gmail.com>
2018-08-06 15:45:03 +0300

    added link between authefication user logic and user logic in library

--- app/user_wrapper.py
+++ app/user_wrapper.py
@@ -2,21 +2,47 @@
 This module contains user wrapper that stores registration data
 for authenticate users in the console interface
 """
-try:
-    from lib.calistra_lib import json_serializer as js
-except ImportError:
-    from calistra_lib import json_serializer as js
-
 import os
+from random import randint
+from datetime import datetime
 
-FILENAME = os.path.join(os.environ['HOME'], 'users.json')
+try:
+    from lib.calistra_lib.storage import json_serializer as js
+    from lib.calistra_lib.user.user_storage import UserStorage
+except ImportError:
+    from calistra_lib.storage import json_serializer as js
+    from calistra_lib.user.user_storage import UserStorage
+
+
+# TODO: в самом начале использования нужно создавать файл с пустым списком!!!
+# TODO: сделать загрузку пользователей из базы данных
+FOLDER = os.path.join(os.environ['HOME'], 'calistra')
+FILENAME = os.path.join(FOLDER, 'auth.json')
+ONLINE = os.path.join(FOLDER, 'online_user.json')
+
+
+def _make_uid():
+    now = datetime.today()
+    time_t = datetime.timetuple(now)
+    uid = ''.join(
+        [
+            str(time_t.tm_year),
+            str(time_t.tm_mon),
+            str(time_t.tm_mday),
+            str(time_t.tm_hour),
+            str(time_t.tm_min),
+            str(time_t.tm_sec),
+            str(randint(0, 1000))
+        ]
+    )
+    return int(uid)
 
 
 class UserWrapper:
     def __init__(self, nick, password):
         self.nick = nick
         self.password = password
-        self.online = False
+        self.uid = _make_uid()
 
     def __eq__(self, other):
         return type(other) == type(self) and self.nick == other.nick and self.password == other.password
@@ -24,23 +50,16 @@ class UserWrapper:
     def __ne__(self, other):
         return type(other) != type(self) or self.nick != other.nick or self.password != other.password
 
-    def change_online_status(self):
-        self.online = not self.online
-
 
 class UserWrapperStorage:
 
     def __init__(self):
         self.users = []
         self.load_users()
+        self.users_storage = UserStorage()
 
     def load_users(self):
-        with open(FILENAME, 'r') as file:
-            s = file.read()
-        self.users = js.from_json([UserWrapper], s)
-
-    # TODO: в самом начале использования нужно создавать файл с пустым списком!!!
-    # TODO: сделать загрузку пользователей из базы данных
+        self.users = js.load([UserWrapper], FILENAME)
 
     def add_user(self, new_user):
         for user in self.users:
@@ -49,17 +68,22 @@ class UserWrapperStorage:
                     "User with nick {} already exists".format(user.nick))
 
         self.users.append(new_user)
+        self.users_storage.add_user(new_user.uid)
 
     def record_users(self):
-        with open(FILENAME, 'w') as file:
-            file.write(js.to_json(self.users))
-
-    # TODO: сделать запись пользователей в базу данных
+        js.unload(self.users, FILENAME)
 
     def get_online_user(self):
+        uid = js.load([], ONLINE)
         for user in self.users:
-            if user.online:
+            if user.uid == uid:
                 return user
+
+    @staticmethod
+    def set_online_user(user=None):
+        if user is None:
+            return js.unload(0, ONLINE)
+        js.unload(user.uid, ONLINE)
 
     def get_user(self, query):
         for user in self.users:
@@ -89,16 +113,13 @@ class UserWrapperController:
         if user is None:
             raise LoginError('Unable to log in. Please check that you have'
                              ' entered your login and password correctly. ')
-        user.change_online_status()
-        self.user_storage.record_users()
+        self.user_storage.set_online_user(user)
 
     def logout(self) -> None:
         user = self.user_storage.get_online_user()
         if not user:
             raise LogoutError("Unable to log out: There are no online users.")
-        user.change_online_status()
-        # TODO: добавить сохранение состояния
-        self.user_storage.record_users()
+        self.user_storage.set_online_user()
 
     def set_new_nick(self, user: UserWrapper):
         pass

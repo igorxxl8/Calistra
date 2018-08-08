209fbfc8db6e7629af8e244ea8faaeaeaf45ae1d
igorxxl <vip.turcevich3@gmail.com>
2018-08-08 16:37:43 +0300

    added storage base class

---
+++ lib/calistra_lib/storage/database.py
@@ -0,0 +1,10 @@
+class Database:
+    def __init__(self, cls_seq, filename):
+        self.cls_seq = cls_seq
+        self.filename = filename
+
+    def load(self):
+        pass
+
+    def unload(self, instance):
+        pass

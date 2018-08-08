7511e02ebd5a9d2904ef6e2cfe4f4bbebfb4c79d
igorxxl <vip.turcevich3@gmail.com>
2018-08-08 14:48:33 +0300

    minor fixes of load and unload tasks from db

--- lib/calistra_lib/task/task_storage.py
+++ lib/calistra_lib/task/task_storage.py
@@ -1,23 +1,23 @@
-import os
 from .task import Task
+
 try:
     from lib.calistra_lib.storage import json_serializer as js
 except ImportError:
     from calistra_lib.storage import json_serializer as js
 
-TASKS_FILE = os.path.join(os.environ['HOME'], 'calistra_data', 'tasks.json')
+# TODO: обобщить метод загрузки данных
 
 
 class TaskStorage:
-    def __init__(self):
-        self.tasks = []
-        self.load_tasks()
+    def __init__(self, tasks_db):
+        self.tasks_db = tasks_db
+        self.tasks = self.load_tasks()
 
     def load_tasks(self):
-        self.tasks = js.load([Task] * 100, TASKS_FILE)
+        return js.load([Task] * 100, self.tasks_db)
 
     def add_task(self, task):
         self.tasks.append(task)
 
     def record_tasks(self):
-        js.unload(self.tasks, TASKS_FILE)
+        js.unload(self.tasks, self.tasks_db)

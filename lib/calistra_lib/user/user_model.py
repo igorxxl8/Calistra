df82387e17ed0ebc5d12ba47cfaede5b8d983048
igorxxl <vip.turcevich3@gmail.com>
2018-08-08 14:47:24 +0300

    added calculate uid inside library user model

--- lib/calistra_lib/user/user_model.py
+++ lib/calistra_lib/user/user_model.py
@@ -1,3 +1,25 @@
+from random import randint
+from datetime import datetime
+
+
 class User:
-    def __init__(self, uid):
-        self.uid = uid
+    def __init__(self, nick):
+        self.uid = _make_uid()
+        self.nick = nick
+
+
+def _make_uid():
+    now = datetime.today()
+    time_t = datetime.timetuple(now)
+    uid = ''.join(
+        [
+            str(time_t.tm_year),
+            str(time_t.tm_mon),
+            str(time_t.tm_mday),
+            str(time_t.tm_hour),
+            str(time_t.tm_min),
+            str(time_t.tm_sec),
+            str(randint(0, 1000))
+        ]
+    )
+    return int(uid)

6cdb75b34c636ae10f0f1d0a892de34672f4b7a2
igorxxl <vip.turcevich3@gmail.com>
2018-08-13 09:45:38 +0300

    renamed functions, added couple of tests

--- lib/calistra_lib_tests/json_tests.py
+++ lib/calistra_lib_tests/json_tests.py
@@ -1,46 +1,46 @@
 import unittest
-from calistra_lib.storage.json_serializer import to_json
+from calistra_lib.storage.json_serializer import to_json, from_json
 
 
 class JsonTests(unittest.TestCase):
 
-    def test_int(self):
+    def test_int_to_json(self):
         number = 2018
         expected = "2018"
         actual = to_json(number)
         self.assertEqual(expected, actual)
 
-    def test_empthy_string(self):
+    def test_empthy_string_to_json(self):
         string = ''
         expected = '""'
         actual = to_json(string)
         self.assertEqual(expected, actual)
 
-    def test_str_1(self):
+    def test_str_1_to_json(self):
         string = "Hello World"
         expected = '"Hello World"'
         actual = to_json(string)
         self.assertEqual(expected, actual)
 
-    def test_str_2(self):
+    def test_str_2_to_json(self):
         string = "\"foo\bar"
         expected = r'"\"foo\bar"'
         actual = to_json(string)
         self.assertEqual(expected, actual)
 
-    def test_str_3(self):
+    def test_str_3_to_json(self):
         string = '\u1234'
         expected = r'"\u1234"'
         actual = to_json(string)
         self.assertEqual(expected, actual)
 
-    def test_array_1(self):
+    def test_array_1_to_json(self):
         array = ['foo', {'bar': ('baz', None, 1.0, 2)}]
         expected = '["foo", {"bar": ["baz", null, 1.0, 2]}]'
         actual = to_json(array)
         self.assertEqual(expected, actual)
 
-    def test_random_object_1(self):
+    def test_random_object_1_to_json(self):
         class RandomTestInstance:
             def __init__(self, first, second, third, four):
                 self.first = first
@@ -67,6 +67,12 @@ class JsonTests(unittest.TestCase):
             is_right = True
 
         self.assertEqual(True, is_right)
+
+    def test_dict_in_list_from_json(self):
+        json_str = '[{"name": "igor"}]'
+        actual = from_json([dict], json_str)
+        expected = [{'name': 'igor'}]
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':

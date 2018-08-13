6e016f86a245a63e3ed24d464a26d457b1a53bb3
igorxxl <vip.turcevich3@gmail.com>
2018-08-13 10:01:38 +0300

    setting attributes for task

--- lib/calistra_lib/task/task.py
+++ lib/calistra_lib/task/task.py
@@ -1,3 +1,26 @@
+# TODO: 1) дописать документацию
+# TODO: 2) Рефакторинг
+# TODO: 3) Логгирование
+
+
 class Task:
-    def __init__(self, task_attr):
-        self.author = task_attr['author']
+    def __init__(self, name, description, parent, linked, author,
+                 responsible: list, priority, progress, start, deadline,
+                 tags, reminder, key):
+        self.name = name
+        self.description = description
+        self.parent = parent
+        self.linked = linked
+        self.author = author
+        if responsible is None:
+            self.responsible = []
+        else:
+            self.responsible = responsible
+        self.priority = priority
+        self.progress = progress
+        self.start = start
+        self.deadline = deadline
+        self.tags = tags
+        self.reminder = reminder
+        self.status = 'opened'
+        self.key = key

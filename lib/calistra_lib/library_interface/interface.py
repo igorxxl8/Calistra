3c97fc6f442c8b22d87029eecc27a044bb9b22bf
igorxxl <vip.turcevich3@gmail.com>
2018-08-16 09:44:06 +0300

    refactoring, some logic migrated in task and user controllers

--- lib/calistra_lib/library_interface/interface.py
+++ lib/calistra_lib/library_interface/interface.py
@@ -1,177 +1,225 @@
 try:
     from lib.calistra_lib.task.task_storage import TaskStorage
+    from lib.calistra_lib.task.task_controller import TaskController
+    from lib.calistra_lib.task.task_exceptions import QueueError, TaskError
+    from lib.calistra_lib.user.user_controller import UserController
     from lib.calistra_lib.user.user_storage import UserStorage
+    from lib.calistra_lib.user.user import User
 except ImportError:
     from calistra_lib.task.task_storage import TaskStorage
+    from calistra_lib.task.task_controller import TaskController
+    from calistra_lib.task.task_exceptions import QueueError, TaskError
     from calistra_lib.user.user_storage import UserStorage
-
+    from calistra_lib.user.user_controller import UserController
+    from calistra_lib.user.user import User
+from datetime import datetime as dt
 
 # TODO: 1) дописать документацию
 # TODO: 2) Рефакторинг
 # TODO: 3) Логирование
 
 
-#                        Add    Set     Show
-# TODO: 1) Name            +     +
-# TODO: 2) Descrip         +
-# TODO: 3) Author          +     -
+#                        Add    Set     Show    Logic
+# TODO: 1) Name            +     +                +
+# TODO: 2) Descrip         +     +                +
+# TODO: 3) Author          +    (-)
 # TODO: 4) Priority        +     +
-# TODO: 5) Progress        +
-# TODO: 6) Start
-# TODO: 7) Deadline
+# TODO: 5) Progress        +     +
+# TODO: 6) Start           +     +                +
+# TODO: 7) Deadline        +     +
 # TODO: 8) Tags            +     +
-# TODO: 9) Status
+# TODO: 9) Status         (-)    +
 # TODO: 10) Reminder
-# TODO: 11) Responsible
-# TODO: 12) Parent
+# TODO: 11) Responsible _работаю
+# TODO: 12) Parent         +     +
 # TODO: 13) linked
-
-
+# TODO: запретить создание таск без имени
+# TODO: рассылка уведомлений ответственным при добавлении и изменении задач
 # TODO: написать перемещение задач в архив при выполнении
 # TODO: перемещениче задач в проваленные при прошедшем дедлайне
+TIME_FORMAT = '%d.%m.%Y.%H:%M'
+
+
+def get_date(string):
+    return dt.strptime(string, TIME_FORMAT)
 
 
 class Interface:
-    def __init__(self, online_user, user_db, task_db):
-        self.online_user = online_user
-        self.users_storage = UserStorage(user_db)
-        self.tasks_storage = TaskStorage(task_db)
+
+    def __init__(self, online_user, users_db, task_db):
+        self.task_controller = TaskController(TaskStorage(task_db))
+        self.user_controller = UserController(UserStorage(users_db))
+        self.online_user = self.user_controller.find_user(nick=online_user)
+        if self.online_user is None:
+            self.online_user = User('guest')
+            self.online_user.uid = '0'
 
     def add_user(self, nick):
-        self.users_storage.add_user(nick)
-        self.add_queue('inbox', nick)
+        new_user = self.user_controller.add_user(nick)
+        self.add_queue('default', new_user.uid, new_user)
+        return new_user
 
-    def add_queue(self, name, owner=None):
-        if not owner:
-            fullname = ''.join([str(self.online_user), "_", name])
+    def add_queue(self, name, key, owner=None):
+        if owner is None:
+            owner = self.online_user
+        if owner.nick == 'guest':
+            raise QueueError('You cannot add new queue. Please sign in system')
+        try:
+            new_queue = self.task_controller.add_queue(name, key, owner)
+        except QueueError as e:
+            raise QueueError(e)
         else:
-            fullname = ''.join([str(owner), "_", name])
-        added_queue = self.tasks_storage.add_queue(fullname)
-        if added_queue == 1:
-            raise QueueError('Queue "{}" already exists'.format(name))
-        return added_queue
+            self.user_controller.link_queue_with_user(owner, new_queue)
+            return new_queue
 
-    def edit_queue(self, name, new_name):
-        fullname = ''.join([str(self.online_user), "_", name])
-        new_fullname = ''.join([str(self.online_user), "_", new_name])
-        queue = self.tasks_storage.get_queue_by_name(fullname)
-        if not queue:
-            raise QueueError('Unable to set new name: '
-                             'Queue "{}" didn\'t found'.format(name))
-        queue.name = new_fullname
-        self.tasks_storage.record_tasks()
-        return queue
+    def edit_queue(self, key, new_name):
+        editor = self.online_user
+        try:
+            return self.task_controller.edit_queue(key, new_name, editor)
+        except QueueError as e:
+            raise QueueError(e)
 
-    def del_queue(self, name, recursive):
-        if name == "inbox":
-            raise QueueError('Unable to delete inbox queue!')
-        fullname = ''.join([str(self.online_user), "_", name])
-        deleted_queue = self.tasks_storage.del_queue(fullname, recursive)
-        if deleted_queue == 2:
-            raise QueueError('Queue "{}" isn\'t empty. '
-                             'Delete all queue tasks or '
-                             'delete queue recursively'.format(name))
-        if deleted_queue == 1:
-            raise QueueError('Queue "{}" didn\'t found.'.format(name))
-        return deleted_queue
+    def del_queue(self, key, recursive):
+        owner = self.online_user
+        try:
+            queue = self.task_controller.del_queue(key, recursive, owner)
+        except QueueError as e:
+            raise QueueError(e)
+        else:
+            self.user_controller.unlink_queue_and_user(owner, queue)
+            print(queue.tasks)
+            if queue.tasks:
+                # TODO: не оповещает пользователей задач которые не подзадачи
+                self.unlink_all_users(queue.tasks, owner)
+            return queue
 
     def get_user_queues(self):
-        queues = []
-        for queue in self.tasks_storage.tasks_queues:
-            if self.online_user == queue.name.split('_')[0]:
-                queues.append(queue)
-        return queues
+        try:
+            queues = self.task_controller.get_queues_by_owner(self.online_user)
+        except QueueError as e:
+            raise QueueError(e)
+        else:
+            return queues
 
-    def get_queue_tasks(self, queue_name):
-        queue_fullname = ''.join([str(self.online_user), "_", queue_name])
-        queue = self.tasks_storage.get_queue_by_name(queue_fullname)
-        if queue is None:
-            raise QueueError('Queue "{}" didn\'t found.'.format(queue_name))
-        return queue.tasks
+    def get_queue_tasks(self, queue_key):
+        owner = self.online_user
+        try:
+            tasks = self.task_controller.get_queue_tasks(queue_key, owner)
+        except QueueError as e:
+            raise QueueError(e)
+        else:
+            return tasks
 
-    def add_task(self, name, queue_name, description, parent, linked,
+    def add_task(self, name, queue_key, description, parent, linked,
                  responsible, priority, progress, start, deadline, tags,
                  reminder, key):
 
-        queue_fullname = ''.join([self.online_user, "_", queue_name])
-        added_task = self.tasks_storage.add_task(
-            author=self.online_user, name=name, queue_name=queue_fullname,
-            description=description, parent=parent, linked=linked,
-            responsible=responsible, priority=priority, progress=progress,
-            start=start, deadline=deadline, tags=tags,
-            reminder=reminder, key=key)
+        author = self.online_user
+        try:
+            responsible_users = self.get_responsible_users(responsible)
 
-        if added_task == 1:
-            raise TaskError('Queue "{}" did not found'.format(queue_name))
-        # TODO: добавить оповещение ответственных
-        return added_task
+            new_task = self.task_controller.add_task(
+                author=author, name=name, queue_key=queue_key,
+                description=description, parent=parent, linked=linked,
+                responsible=responsible, priority=priority, progress=progress,
+                start=start, deadline=deadline, tags=tags, reminder=reminder,
+                key=key
+            )
+        except TaskError as e:
+            raise TaskError(e)
+        else:
+            for user in responsible_users:
+                self.user_controller.link_task_with_responsible(
+                    responsible=user, task=new_task)
+            if self.online_user.nick != 'guest':
+                self.user_controller.link_task_with_author(author, new_task)
+            return new_task
 
     def edit_task(self, key, name, description, parent, linked,
                   responsible, priority, progress, start, deadline, tags,
                   reminder, status):
+        editor = self.online_user
+        try:
+            edited_task = self.task_controller.find_task(key=key)
+            old_responsible_users = self.get_responsible_users(
+                edited_task.responsible)
+            new_responsible_users = self.get_responsible_users(responsible)
+            task = self.task_controller.edit_task(
+                key=key, editor=editor, name=name, description=description,
+                parent=parent, linked=linked, priority=priority,
+                progress=progress, start=start, deadline=deadline, tags=tags,
+                reminder=reminder, status=status, responsible=responsible)
 
-        task = self.tasks_storage.get_task_by_key(key)
-        if task is None:
-            raise TaskError('Task with key "{}" didn\'t found'.format(key))
-
-        online_user = self.online_user
-        # TODO: разрешить редактировать только автору
-        if (online_user not in task.responsible and
-                online_user != task.author):
-
-            raise TaskError('Access denied: '
-                            'You can not '
-                            'edit this task'.format(online_user))
-
-        if (online_user in task.responsible and
-                online_user != task.author and
-                (name or description or parent or linked or responsible or
-                 priority or start or deadline or tags or reminder)):
-
-            raise TaskError('You can not edit task param '
-                            'besides status and progress')
-
-        if progress:
-            task.progress = progress
-        if status:
-            task.status = status
-        if name:
-            task.name = name
-        if description:
-            task.description = description
-        if parent:
-            task.parent = parent
-        if linked:
-            task.linked = linked
-        if responsible:
-            task.responsible = responsible
-        if priority:
-            task.priority = priority
-        if start:
-            task.start = start
-        if deadline:
-            task.deadline = deadline
-        if tags:
-            if tags == '?':
-                task.tags = None
-            else:
-                task.tags = tags
-        if reminder:
-            task.reminder = reminder
-        self.tasks_storage.record_tasks()
-        # TODO: оповещение ответственных
+        except TaskError as e:
+            raise TaskError(e)
+        else:
+            unresp, resp = self.get_responsible_diff(
+                new_responsible_users, old_responsible_users)
+            new_responsible_users = self.get_responsible_users(resp)
+            old_responsible_users = self.get_responsible_users(unresp)
+            # TODO: где то здесь неправильно пишется в json, найти и исправить
+            for user in old_responsible_users:
+                self.user_controller.unlink_task_and_responsible(
+                    responsible_nick=user.nick, task=task
+                )
+            for user in new_responsible_users:
+                self.user_controller.link_task_with_responsible(
+                    responsible=user, task=task)
+            return task
 
     def del_task(self, key, recursive):
-        task = self.tasks_storage.get_task_by_key(key)
-        if task is None:
-            raise TaskError('Task with key "{}" didn\'t found'.format(key))
-        deleted_taks = self.tasks_storage.del_task(task, recursive)
-        if deleted_taks is None:
-            raise TaskError('Can not delete task. It has subtasks. '
-                            'Delete all subtasks or delete task recursively')
-        self.tasks_storage.record_tasks()
-        # TODO: добавить уведомлении о удалении задачи ответственным
+        owner = self.online_user
+        try:
+            tasks = self.task_controller.del_task(owner, key, recursive)
+        except TaskError as e:
+            raise TaskError(e)
+        else:
+            self.unlink_all_users(tasks, owner)
+            return tasks
 
+    def unlink_responsible(self, task):
+        while task.responsible:
+            self.user_controller.unlink_task_and_responsible(
+                responsible_nick=task.responsible.pop(), task=task)
+
+    def unlink_author(self, author, task):
+        if author is not None:
+            self.user_controller.unlink_task_and_author(author, task)
+
+    def unlink_all_users(self, tasks, owner=None):
+        for task in tasks:
+            self.unlink_responsible(task)
+            self.unlink_author(owner, task)
+
+    def get_responsible_diff(self, new, old):
+        str_olds = [user.nick for user in old]
+        str_news = [user.nick for user in new]
+        diff_old = []
+        diff_new = []
+        for str_old in str_olds:
+            if str_old not in str_news:
+                diff_old.append(str_old)
+        for str_new in str_news:
+            if str_new not in str_olds:
+                diff_new.append(str_new)
+        return diff_old, diff_new
+
+    def get_responsible_users(self, responsible):
+        responsible_users = []
+        if responsible == '?':
+            return responsible_users
+        if responsible:
+            for person in responsible:
+                user = self.user_controller.find_user(nick=person)
+                if user is None:
+                    raise TaskError(
+                        'User "{}" didn\'t found'.format(person))
+                responsible_users.append(user)
+        return responsible_users
+
+    #
+    #
+    # Приступить после окончания работы с задачами
     def add_plan(self):
         pass
 
@@ -180,13 +228,3 @@ class Interface:
 
     def edit_plan(self):
         pass
-
-
-class QueueError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
-class TaskError(Exception):
-    def __init__(self, message):
-        self.message = message

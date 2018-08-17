28211b5cb5e38dfa98236a1d92a459446488e08c
igorxxl <vip.turcevich3@gmail.com>
2018-08-17 20:38:29 +0300

    queue store fail tasks

--- lib/calistra_lib/task/queue.py
+++ lib/calistra_lib/task/queue.py
@@ -4,9 +4,10 @@
 
 
 class Queue:
-    def __init__(self, name, key, owner, tasks, archive):
+    def __init__(self, name, key, owner, opened, solved):
         self.name = name
         self.key = key
         self.owner = owner
-        self.tasks = tasks
-        self.archive = archive
+        self.opened = opened
+        self.solved = solved
+        self.failed = []

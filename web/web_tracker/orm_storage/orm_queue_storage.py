dea9df354674b1a664266c49e6ea187e7b44ad63
igorxxl <vip.turcevich3@gmail.com>
2018-09-05 01:25:47 +0300

    added method for find queue in storage

--- web/web_tracker/orm_storage/orm_queue_storage.py
+++ web/web_tracker/orm_storage/orm_queue_storage.py
@@ -1,5 +1,6 @@
 from calistra_lib.queue.queue_storage_interface import IQueueStorage
 from web_tracker.models import Queue
+from django.shortcuts import get_object_or_404
 
 
 class ORMQueueStorage(IQueueStorage):
@@ -21,10 +22,21 @@ class ORMQueueStorage(IQueueStorage):
         self.queues.filter(key=queue.key).delete()
 
     def get_queue_by_key(self, key):
-        return self.queues.get(key=key)
+        try:
+            return self.queues.get(key=key)
+        except Exception:
+            return None
+
+    def find_queue(self, key=None, name=None, owner=None):
+        queue = self.get_queue_by_key(key)
+        if owner and queue is None:
+            return self.get_user_default_queue(owner)
+        if name and queue is None:
+            return self.get_queue_by_name(name)
+        return queue
 
     def get_queue_by_name(self, name):
         return self.queues.get(name=name)
 
     def get_user_default_queue(self, user):
-        return self.queues.get(owner=user.uid)
+        return self.queues.filter(owner=user.uid).get(name='default')

5575b9baa724d18930ac4348349ca46782742943
igorxxl <vip.turcevich3@gmail.com>
2018-09-05 01:24:56 +0300

    in some models changed default values and max length

--- web/web_tracker/models.py
+++ web/web_tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .validators import validate_reminder
 
 
 # Create your models here.
@@ -6,52 +7,47 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    queue = models.CharField(max_length=10)
+    queue = models.CharField(max_length=30)
     description = models.TextField(default='')
-    parent = models.CharField(max_length=10)
-    sub_tasks = models.TextField(default='[]')
-    related = models.CharField(max_length=10)
+    parent = models.CharField(max_length=30)
+    sub_tasks = models.TextField(default='')
+    related = models.CharField(max_length=30)
     author = models.CharField(max_length=100)
     priority = models.IntegerField(default=0)
     progress = models.IntegerField(default=0)
     start = models.CharField(max_length=30)
     deadline = models.CharField(max_length=30)
-    tags = models.TextField(default='[]')
-    reminder = models.TextField(default='')
-    status = models.CharField(max_length=10)
-    key = models.CharField(max_length=10)
+    tags = models.TextField(default='')
+    reminder = models.TextField(
+        default='',
+        validators=[validate_reminder])
+    status = models.CharField(max_length=30)
+    key = models.CharField(max_length=30)
     creating_time = models.CharField(max_length=30)
     editing_time = models.CharField(max_length=30)
-    responsible = models.TextField(default='[]')
+    responsible = models.TextField(default='')
 
     objects = models.Manager()
-
-    # @property
-    # def sub_tasks(self):
-    #     return eval(str(self.__sub_tasks))
-    #
-    # @sub_tasks.setter
-    # def sub_tasks(self, value):
-    #     self.__sub_tasks = models.TextField(str(value))
 
 
 class Plan(models.Model):
     author = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
+    period = models.CharField(max_length=30, default='')
     time = models.CharField(max_length=30)
     reminder = models.TextField(default='')
-    key = models.CharField(max_length=10)
+    key = models.CharField(max_length=30)
 
     objects = models.Manager()
 
 
 class Queue(models.Model):
     name = models.CharField(max_length=100)
-    key = models.CharField(max_length=10)
+    key = models.CharField(max_length=30)
     owner = models.IntegerField(0)
-    opened_tasks = models.TextField(default='[]')
-    failed_tasks = models.TextField(default='[]')
-    solved_tasks = models.TextField(default='[]')
+    opened_tasks = models.TextField(default='')
+    failed_tasks = models.TextField(default='')
+    solved_tasks = models.TextField(default='')
 
     objects = models.Manager()
     # TODO: закончить модели
@@ -60,8 +56,8 @@ class Queue(models.Model):
 class User(models.Model):
     uid = models.IntegerField(0)
     nick = models.CharField(max_length=100)
-    queues = models.TextField(default='[]')
-    tasks_author = models.TextField(default='[]')
-    tasks_responsible = models.TextField(default='[]')
-    notifications = models.TextField(default='[]')
-    new_messages = models.TextField(default='[]')
+    queues = models.TextField(default='')
+    tasks_author = models.TextField(default='')
+    tasks_responsible = models.TextField(default='')
+    notifications = models.TextField(default='')
+    new_messages = models.TextField(default='')

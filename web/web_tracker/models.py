58ab33e4a414137d14d7f2c581322eed22951d31
igorxxl <vip.turcevich3@gmail.com>
2018-09-04 10:25:22 +0300

    added new models and views functions

--- web/web_tracker/models.py
+++ web/web_tracker/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    queue = models.CharField(max_length=10)
+    description = models.TextField(default='')
+    parent = models.CharField(max_length=10)
+    sub_tasks = models.TextField(default='[]')
+    related = models.CharField(max_length=10)
+    author = models.CharField(max_length=100)
+    priority = models.IntegerField(default=0)
+    progress = models.IntegerField(default=0)
+    start = models.CharField(max_length=30)
+    deadline = models.CharField(max_length=30)
+    tags = models.TextField(default='[]')
+    reminder = models.TextField(default='')
+    status = models.CharField(max_length=10)
+    key = models.CharField(max_length=10)
+    creating_time = models.CharField(max_length=30)
+    editing_time = models.CharField(max_length=30)
+    responsible = models.TextField(default='[]')
+
+    objects = models.Manager()
+
+    # @property
+    # def sub_tasks(self):
+    #     return eval(str(self.__sub_tasks))
+    #
+    # @sub_tasks.setter
+    # def sub_tasks(self, value):
+    #     self.__sub_tasks = models.TextField(str(value))
+
+
+class Plan(models.Model):
+    author = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    time = models.CharField(max_length=30)
+    reminder = models.TextField(default='')
+    key = models.CharField(max_length=10)
+
+    objects = models.Manager()
+
+
+class Queue(models.Model):
+    name = models.CharField(max_length=100)
+    key = models.CharField(max_length=10)
+    owner = models.IntegerField(0)
+    opened_tasks = models.TextField(default='[]')
+    failed_tasks = models.TextField(default='[]')
+    solved_tasks = models.TextField(default='[]')
+
+    objects = models.Manager()
+    # TODO: закончить модели
+
+
+class User(models.Model):
+    uid = models.IntegerField(0)
+    nick = models.CharField(max_length=100)
+    queues = models.TextField(default='[]')
+    tasks_author = models.TextField(default='[]')
+    tasks_responsible = models.TextField(default='[]')
+    notifications = models.TextField(default='[]')
+    new_messages = models.TextField(default='[]')
--- web/web_tracker/views.py
+++ web/web_tracker/views.py
@@ -1,3 +1,164 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import TaskForm, QueueForm, PlanForm
+import os
+import uuid
+
+from .orm_storage.orm_task_storage import ORMTaskStorage
+from .orm_storage.orm_queue_storage import ORMQueueStorage
+from .orm_storage.orm_plan_storage import ORMPlanStorage
+from .orm_storage.orm_user_storage import ORMUserStorage
+from calistra_lib.task.task_controller import TaskController
+from calistra_lib.plan.plan_controller import PlanController
+from calistra_lib.queue.queue_controller import QueueController
+from calistra_lib.user.user_controller import UserController
+from calistra_lib.interface import Interface
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+task_storage = ORMTaskStorage()
+task_controller = TaskController(task_storage)
+user_storage = ORMUserStorage()
+user_controller = UserController(user_storage)
+plan_storage = ORMPlanStorage()
+plan_controller = PlanController(plan_storage)
+queue_storage = ORMQueueStorage()
+queue_controller = QueueController(queue_storage)
+interface = Interface('', queue_controller, user_controller,
+                      task_controller,
+                      plan_controller)
+
+DEL = 100000000000000000000
+
+
+def online_user_setter(func):
+    def wrapper_func(request):
+        interface.set_online_user(request.user.username)
+        print(interface.online_user.nick)
+        return func(request)
+
+    return wrapper_func
+
+
+def title(request):
+    if request.user.is_authenticated():
+        return dashboard(request)
+    else:
+        return index(request)
+
+
+def index(request):
+    return render(request, 'web_tracker/index.html')
+
+
+@online_user_setter
+@login_required
+def dashboard(request):
+    user = interface.get_online_user()
+    queues = queue_storage.queues.filter(owner=user.uid)
+    author_tasks, responsible_tasks = interface.get_user_tasks()
+    context = {'queues': queues, 'author_tasks': author_tasks,
+               'responsible_tasks': responsible_tasks}
+    return render(request, 'web_tracker/dashboard.html', context)
+
+
+@online_user_setter
+@login_required
+def new_task(request):
+    if request.method != 'POST':
+        form = TaskForm()
+    else:
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            interface.create_task(
+                name=form.cleaned_data['name'],
+                description=form.cleaned_data['description'],
+                queue_key=form.cleaned_data['queue'],
+                parent=None,
+                related=form.cleaned_data['related'],
+                responsible=None,
+                priority=form.cleaned_data['priority'],
+                progress=form.cleaned_data['progress'],
+                start=form.cleaned_data['start'],
+                deadline=form.cleaned_data['deadline'],
+                tags=form.cleaned_data['tags'],
+                reminder=form.cleaned_data['reminder'],
+                key=os.urandom(6).hex())
+            return HttpResponseRedirect(reverse('web_tracker:dashboard'))
+    context = {'form': form}
+    return render(request, 'web_tracker/new_task.html', context)
+
+
+@online_user_setter
+@login_required
+def show_task(request):
+    if request.method == 'GET':
+        form = TaskForm(data=request.GET)
+        context = {'form': form}
+        return render(request, 'web_tracker/show_task.html', context)
+
+
+@online_user_setter
+@login_required
+def new_queue(request):
+    if request.method != 'POST':
+        form = QueueForm()
+    else:
+        form = QueueForm(data=request.POST)
+        if form.is_valid():
+            interface.add_queue(name=form.cleaned_data['name'],
+                                key=os.urandom(6))
+            return HttpResponseRedirect(reverse('web_tracker:dashboard'))
+    context = {'form': form}
+    return render(request, 'web_tracker/new_queue.html', context)
+
+
+@online_user_setter
+@login_required
+def create_plan(request):
+    if request.method != 'POST':
+        form = PlanForm()
+    else:
+        form = PlanForm(data=request.POST)
+        if form.is_valid():
+            interface.add_plan(key=os.urandom(7),
+                               name=form.cleaned_data['name'],
+                               time=form.cleaned_data['time'],
+                               reminder=form.cleaned_data['reminder'])
+            return HttpResponseRedirect(reverse('web_tracker:dashboard'))
+    context = {'form': form}
+    return render(request, 'web_tracker/create_plan.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('web_tracker:index'))
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(
+                username=new_user.username,
+                password=request.POST['password1']
+            )
+            interface.add_user(
+                new_user.username,
+                uid=uuid.uuid4().int // DEL,
+                queue_key=os.urandom(6).hex()
+            )
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('web_tracker:index'))
+    context = {'form': form}
+
+    return render(request, 'users/register.html', context)
